8c03274d84122bacb2e86d347b10dbc80788d960
JMSF1925 <joaomiguelsf1357@gmail.com>
2026-08-27 13:14:01 +0100

    Try to create the logic of the button
    In this section, the logic for the
    "create new grid" button was created, but it
    doesn't work well yet.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector("#grid");
+const button = document.querySelector("button");
 let x = 0;
 let y = 0;
 let z = 0;
@@ -42,3 +43,58 @@ div.forEach((item) => {
         e.target.style.backgroundColor = "blue";
     })
 });
+
+
+button.addEventListener("click", () => {
+    x = 0;
+    y = 0;
+    z = 0;
+    let squares = prompt("How many squares do you want in the grid (per side)?");
+    let numericSquares = Number(squares);
+    
+    if(Number.isInteger(numericSquares) && numericSquares <= 100)
+    {
+        columns.forEach((item) => {
+            grid.removeChild(item);
+        })
+
+        while(x < numericSquares)
+        {
+            const col = document.createElement("div");
+            col.classList.add("col");
+            col.style.height = "60px";
+            col.style.width = "60px";
+            col.style.backgroundColor = "black";
+            grid.appendChild(col);
+            x++;
+        }
+
+        columns = document.querySelectorAll(".col");
+
+        while(y < numericSquares)
+        {
+            while(z < numericSquares - 1)
+            {
+                const line = document.createElement("div");
+                line.classList.add("line");
+                line.style.height = "60px";
+                line.style.width = "60px";
+                line.style.backgroundColor = "black";
+                columns.item(y).appendChild(line);
+                z++;
+            }   
+
+            z = 0;
+            y++;
+        }
+
+    }
+    else if(Number.isInteger(numericSquares) && numericSquares > 100)
+    {
+        alert("Too high! The limit is 100!");
+    }
+    else
+    {
+        alert("It has to be an interger!")
+    }
+})

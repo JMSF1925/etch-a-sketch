11e6aaacba0a886d59a7a2a0896a6b4a90446d00
JMSF1925 <joaomiguelsf1357@gmail.com>
2026-08-27 11:46:22 +0100

    Add flex to grid lines and columns
    In this section, a flex was added to the grid for
    resize.

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let z = 0;
 while(x < 16)
 {
     const col = document.createElement("div");
-    col.classList.add("col")
+    col.classList.add("col");
     col.style.height = "60px";
     col.style.width = "60px";
     col.style.backgroundColor = "black";
+    col.style.flex = "1";
     grid.appendChild(col);
     x++;
 }
@@ -25,6 +26,7 @@ while(y < 16)
         line.style.height = "60px";
         line.style.width = "60px";
         line.style.backgroundColor = "black";
+        line.style.flex = "1";
         columns.item(y).appendChild(line);
         z++;
     }
